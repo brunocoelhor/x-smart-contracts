48b723122de6e2e16d0c5ab1d3bc79e050a0238b
Bruno Rodrigues <brunocoelho.r@gmail.com>
2025-03-11 17:56:27 +0100

    feat(run.js): Add createPost

--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,12 @@
 const main = async () => {
   const [owner] = await hre.ethers.getSigners();
-  const xPost = await hre.ethers.deployContract("XPost");
+  const xPost = await hre.ethers.deployContract("XPost", {
+    value: hre.ethers.parseEther("0.1"),
+  });
   await xPost.waitForDeployment();
 
-  await xPost.getTotalPosts();
-
-  const waveTxn = await xPost.createPost();
-  await waveTxn.wait();
+  const postTxn = await xPost.createPost("Submitting post #1");
+  await postTxn.wait();
 
   await xPost.getTotalPosts();
 };
