40befc7b325a8f2d0916cc13a74a201f65f26cbc
Bruno Rodrigues <brunocoelho.r@gmail.com>
2025-03-12 15:27:09 +0100

    feat(config): add staging network configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const accounts = process.env.ACCOUNTS.split(',');
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    staging: {
+      url: process.env.URL,
+      accounts: accounts,
+    },
+  }
 };
