29db94a83006c6dbc210a75d4bf2b900a27d9d9a
Bruno Rodrigues <brunocoelho.r@gmail.com>
2025-03-12 15:04:02 +0100

    refactor(deploy.js): adjust gas price retrieval method

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,20 @@
 const main = async () => {
-    const [owner] = await hre.ethers.getSigners();
-    const accountBalance = await owner.provider.getBalance(owner.address);
-  
-    console.log("Deploy do contrato feito por:", owner.address);
-    console.log("Saldo da conta:", accountBalance.toString());
-  
-    const xPost = await hre.ethers.deployContract("XPost");
-    await xPost.waitForDeployment();
-  
-    console.log("Deploy do contrato no endereço:", xPost.target);
+  const [owner] = await hre.ethers.getSigners();
+
+  const xPost = await hre.ethers.deployContract("XPost", {
+    value: hre.ethers.parseEther("0.1"),
+  });
+  await xPost.waitForDeployment();
   };
-  
+
   const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
-  
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
